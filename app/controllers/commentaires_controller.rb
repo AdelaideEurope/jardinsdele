d7e2cdf4d64ab2b90ae0101a37a86f1f108a79be
Adelaide Blot <adelaide.blot@hotmail.fr>
2020-03-22 23:44:14 +0100

    ajout activité dans le scope de la recherche sur les commentaires

--- app/controllers/commentaires_controller.rb
+++ app/controllers/commentaires_controller.rb
@@ -2,7 +2,7 @@ class CommentairesController < ApplicationController
   def index
     if params[:query]
       @commentaires_all = Commentaire.all
-      @commentaires = @commentaires_all.search(params[:query])
+      @commentaires = @commentaires_all.global_search(params[:query])
     else
       @commentaires = Commentaire.all
     end
--- app/models/commentaire.rb
+++ app/models/commentaire.rb
@@ -2,10 +2,12 @@ class Commentaire < ApplicationRecord
   belongs_to :activite, optional: true
   validates_presence_of :activite
   include PgSearch::Model
-  pg_search_scope :search,
-    against: [ :description ],
+  pg_search_scope :global_search,
+    against: [:description],
+    associated_against: {
+      activite: [:nom]
+    },
     using: {
       tsearch: { prefix: true }
     }
 end
-
