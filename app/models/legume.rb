b3d3c6f43aac8ec0b3a171cafeed82789519811c
Adelaide Blot <adelaide.blot@hotmail.fr>
2020-03-28 11:16:16 +0100

    rendre ajout d'une famille obligatoire lors de la création d'un légume

--- app/models/legume.rb
+++ app/models/legume.rb
@@ -7,4 +7,8 @@ class Legume < ApplicationRecord
   has_one_attached :photo
   extend FriendlyId
   friendly_id :legume_css, use: :slugged
+  validates :famille, presence: true
+  validates :nom, presence: true
+  validates :legume_css, presence: true
+  validates :prix_general, presence: true
 end
