077f52ca5453464e423c8e11df59a2a5cff6f89a
Adelaide Blot <adelaide.blot@hotmail.fr>
2020-04-24 14:00:39 +0200

    mark task as done

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -24,8 +24,13 @@ class MemosController < ApplicationController
   def update
     @memo = Memo.find(params[:id])
     if @memo.update(memo_params)
-      flash[:notice] = "Note modifiée avec succès !"
-      redirect_to home_path
+      if params[:memo][:done] == "true"
+        flash[:notice] = "Tâche effectuée !"
+        redirect_to home_path
+      else
+        flash[:notice] = "Note modifiée avec succès !"
+        redirect_to home_path
+      end
     else
       render :edit
     end
@@ -34,12 +39,13 @@ class MemosController < ApplicationController
   def destroy
     @memo = Memo.find(params[:id])
     @memo.destroy
+    flash[:notice] = "Note supprimée avec succès !"
     redirect_to home_path
   end
 
   private
 
   def memo_params
-    params.require(:memo).permit(:description, :date, :categorie)
+    params.require(:memo).permit(:description, :date, :categorie, :done)
   end
 end
