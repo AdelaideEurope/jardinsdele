d7d0505bad38a5058612df98cb14d71967431c1f
Adelaide Europe <adelaide.blot@hotmail.fr>
2020-08-15 00:18:16 +0200

    performance legumes index

--- app/controllers/legumes_controller.rb
+++ app/controllers/legumes_controller.rb
@@ -7,11 +7,12 @@ class LegumesController < ApplicationController
     @firsthalf = (@legumes.size / 2.to_f).ceil
     @secondhalf = @legumes.size / 2
 
-    @tous_legumes_parlegume = @legumes.includes(:commentaires, :vente_lignes, :panier_lignes).map { |legume|
+    @tous_legumes = @legumes.includes(:commentaires, :vente_lignes, :panier_lignes).map { |legume|
       { nom: legume.nom, legume_css: legume.legume_css, planches: nb_planches(legume), calegume: calegume(legume), pourcentage_ca: pourcentage_ca(legume), commentaires: legume.commentaires.where.not(description: [nil, ""]), photos: photo?(legume) } }.sort_by { |hashlegume| hashlegume[:legume_css] }
 
-    @tous_legumes_parca = @legumes.map { |legume|
-      { nom: legume.nom, legume_css: legume.legume_css, calegume: calegume(legume), pourcentage_ca: pourcentage_ca(legume), planches: nb_planches(legume), commentaires: legume.commentaires.where.not(description: [nil, ""]), photos: photo?(legume) } }.sort_by { |hashlegume| -hashlegume[:calegume] }
+    @tous_legumes_parlegume = @tous_legumes.sort_by { |hashlegume| hashlegume[:legume_css] }
+
+    @tous_legumes_parca = @tous_legumes.sort_by { |hashlegume| -hashlegume[:calegume] }
 
     @legumes_semaines_graph = @legumes.map { |legume| { name: legume.nom, legume_css: legume.legume_css, data: legumes_semaines_graph(legume), famille: legume.famille.downcase.tr("é", "e") } }.sort_by { |hashlegume| -hashlegume[:name].downcase.tr("é", "e") }
     # familles_colors = { "alliacees" => "#FF6600", "apiacees" => "#99CC33", "asteracees" => "#CC0000", "brassicacees" => "#33CCFF", "chenopodacees" => "#FFCC00", "cucurbitacees" => "#660099", "divers" => "#990000", "fabacees" => "#006633", "solanacees" => "#009999" }
